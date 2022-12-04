f5a58ecad01a80e6ea5fbf2a2777ee24bee84832
EC2 Default User <ec2-user@ip-172-31-14-103.ap-northeast-1.compute.internal>
2022-12-04 07:07:00 +0000

    5-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   has_many :books
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :name, length: {minimum: 2, maximum: 20 }, uniqueness: true
-  validates :introduction, length: {maximum:50} 
+  validates :introduction, length: {maximum:50}
 
 
   def get_profile_image
